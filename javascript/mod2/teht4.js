8cf2cc10a8f2dcb5aa8921d940b9b77dafe0e6b6
patrivar <patric.varis@metropolia.fi>
2024-11-14 20:15:41 +0200

    l

--- javascript/mod2/teht4.js
+++ javascript/mod2/teht4.js
@@ -1,3 +1,23 @@
 // Write a program that asks the user for numbers until he gives zero.
 // The given numbers are printed in the console from the largest to the smallest.
+let number;
+const list = [];
+do {
+    number = parseInt(prompt("Anna numero(0=stop):"))
+    if (number !== 0){
+        list.push(number);
+    }
+} while (number !== 0);
 
+console.log(list);
+list.sort((a, b) => b - a);
+
+console.log("Numerot suurimmasta pienimpään:")
+for (const num of list) {
+    console.log(num);
+}
+for (const numbers of list) {
+    const listElement = document.createElement("li")
+    listElement.textContent = numbers
+    document.body.appendChild(listElement)
+}
