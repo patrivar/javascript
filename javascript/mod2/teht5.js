9dbc7286abc8c055ceb1dd32613c3c9c36ebb798
patrivar <patric.varis@metropolia.fi>
2024-11-14 20:20:25 +0200

    l

---
+++ javascript/mod2/teht5.js
@@ -0,0 +1,6 @@
+// Write a program that prompts the user for numbers.
+// When the user enters one of the numbers he previously entered,
+// the program will announce that the number has already been given
+// and stops its operation and then prints all the given numbers to the
+// console in ascending order.
+
