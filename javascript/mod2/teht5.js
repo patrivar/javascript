4f4fc2a9fbe4dcf91abdf67df135a4dfbef73b95
patrivar <patric.varis@metropolia.fi>
2024-11-18 18:38:11 +0200

    l

--- javascript/mod2/teht5.js
+++ javascript/mod2/teht5.js
@@ -4,3 +4,22 @@
 // and stops its operation and then prints all the given numbers to the
 // console in ascending order.
 
+const numbers = [];
+let number;
+while (true) {
+    number = parseInt(prompt("Enter a number:"));
+    if (numbers.includes(number)) {
+        break;
+    }
+    numbers.push(number);
+} numbers.sort((a, b) => a - b);
+
+for (const num of numbers) {
+console.log(num);
+}
+
+for (const num of numbers) {
+    const listElement = document.createElement("li")
+    listElement.textContent = num;
+    document.body.appendChild(listElement)
+}
---
+++ javascript/mod2/teht6.js
@@ -0,0 +1,20 @@
+// Write a function that returns a random dice roll between 1 and 6.
+// The function should not have any parameters. Write a main program
+// that rolls the dice until the result is 6. The main program should
+// print out the result of each roll in an unordered list (<ul>).
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+document.addEventListener('DOM', function() {
+    const ulElement = document.createElement('ul');
+    document.body.appendChild(ulElement);
+    let roll;
+    do {
+        roll = rollDice();
+        const listItem = document.createElement('li');
+        listItem.textContent = `Heitit: ${roll}`;
+        ulElement.appendChild(listItem);
+    }
+    while (roll !== 6);
+});
