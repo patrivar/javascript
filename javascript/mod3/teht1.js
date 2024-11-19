902e266a2a1ca9ef327a8fb47553acd3aab744c4
patrivar <patric.varis@metropolia.fi>
2024-11-19 18:55:26 +0200

    l

---
+++ javascript/mod3/teht1.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    const targetElement = document.getElementById('target');
+    targetElement.innerHTML =
+        "<li>First item</li> " +
+        "<li>Second item</li> " +
+        "<li>Third item</li>";
+    listElement.classList.add("my-list");
+})
---
+++ javascript/mod3/teht2.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const targetElement = document.getElementById("target");
+    const firstItem = document.createElement("li");
+    firstItem.textContent = "First item";
+    const secondItem = document.createElement("li");
+    secondItem.textContent = "Second item";
+    secondItem.classList.add("my-item");
+    const thirdItem = document.createElement("li");
+    thirdItem.textContent = "Third item";
+    targetElement.appendChild(firstItem);
+    targetElement.appendChild(secondItem);
+    targetElement.appendChild(thirdItem);
+});
